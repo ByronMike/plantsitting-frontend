0235f50e0c30a66b8fc53efae42a68e9f804f514
Mathis <mathis.cn@gmail.com>
2022-12-12 15:08:22 +0100

    react native base

--- App.js
+++ App.js
@@ -2,52 +2,52 @@
 import { LogBox } from "react-native";
 LogBox.ignoreAllLogs();
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LandingScreen from './screens/LandingScreen';
-import Presentation1Screen from './screens/Presentation1Screen';
-import Presentation2Screen from './screens/Presentation2Screen';
-import Presentation3Screen from './screens/Presentation3Screen';
-import Presentation4Screen from './screens/Presentation4Screen';
-import HomeScreen from './screens/HomeScreen';
-import SearchScreen from './screens/SearchScreen';
-
-
-import SignupScreen from './screens/SignupScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NativeBaseProvider, Text, Box } from "native-base";
+import LandingScreen from "./screens/LandingScreen";
+import Presentation1Screen from "./screens/Presentation1Screen";
+import Presentation2Screen from "./screens/Presentation2Screen";
+import Presentation3Screen from "./screens/Presentation3Screen";
+import Presentation4Screen from "./screens/Presentation4Screen";
+import HomeScreen from "./screens/HomeScreen";
+import SearchScreen from "./screens/SearchScreen";
+import SignupScreen from "./screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
- return (
-   <Tab.Navigator screenOptions={{ headerShown: false }}>
-    // TODO Rajouter les tabs 
-     {/* <Tab.Screen name="Accueil" component={HomeScreen} />
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      // TODO Rajouter les tabs
+      {/* <Tab.Screen name="Accueil" component={HomeScreen} />
      <Tab.Screen name="Chercher" component={SearchScreen} />
      <Tab.Screen name="Messages" component={MessagesScreen} />
      <Tab.Screen name="Profile" component={ProfileScreen} /> */}
-   </Tab.Navigator>
- );
-}
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
- return (
-   <NavigationContainer>
-     <Stack.Navigator screenOptions={{ headerShown: false }}>
-       <Stack.Screen name="Landing" component={LandingScreen} />
-       <Stack.Screen name="Presentation1" component={Presentation1Screen} />
-       <Stack.Screen name="Presentation2" component={Presentation2Screen} />
-       <Stack.Screen name="Presentation3" component={Presentation3Screen} />
-       <Stack.Screen name="Presentation4" component={Presentation4Screen} />
-       <Stack.Screen name="Home" component={HomeScreen} />
-       <Stack.Screen name="Search" component={SearchScreen} />
+  return (
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Landing" component={LandingScreen} />
+          <Stack.Screen name="Presentation1" component={Presentation1Screen} />
+          <Stack.Screen name="Presentation2" component={Presentation2Screen} />
+          <Stack.Screen name="Presentation3" component={Presentation3Screen} />
+          <Stack.Screen name="Presentation4" component={Presentation4Screen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Search" component={SearchScreen} />
 
+          <Stack.Screen name="Signup" component={SignupScreen} />
 
-       <Stack.Screen name="Signup" component={SignupScreen} />
-
-       <Stack.Screen name="TabNavigator" component={TabNavigator} />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,82 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { Modal } from "native-base";
+import { useState } from "react";
+import { NativeBaseProvider } from "native-base";
 
 export default function HomeScreen({ navigation }) {
- return (
-   <View style={styles.container}>
-     <Text>Home Screen</Text>
-     <Button
-       title="Go to Landing Screen"
-       onPress={() => navigation.navigate('Landing')}
-     />
-   </View>
- );
+  const [showModal, setShowModal] = useState(false);
+  const [signinUsername, setSigninUsername] = useState("");
+  const [signinPassword, setSigninPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../assets/logo-basic.png")}
+      />
+      <View style={styles.bloctext}>
+        <Text style={styles.textbienvenue}>Hello 👋 </Text>
+        <Text style={styles.textdemande}>Que cherchez-vous aujourd’hui ? </Text>
+      </View>
+      <View style={styles.blocchoix}>
+        <TouchableOpacity style={styles.recherche}>
+          <View style={styles.top}>
+            <Text style={styles.titrerecherche}>
+              Je <Text style={styles.boutoncherche}>CHERCHE</Text> {"\n"}un
+              plant-sitter
+            </Text>
+            <FontAwesome
+              icon="thin"
+              name="arrow-right"
+              size={20}
+              color="#DDA15E"
+            />
+          </View>
+          <View style={styles.bottom}>
+            <Text style={styles.texteinfo}>
+              Je souhaite être mis en relation avec une personne pour garder mes
+              plantes, ou entretenir mon jardin.
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.recherche}>
+          <View style={styles.top}>
+            <Text style={styles.titrerecherche}>
+              Je <Text style={styles.boutoncherche}>SUIS</Text> {"\n"}un
+              plant-sitter
+            </Text>
+            <FontAwesome
+              icon="thin"
+              name="arrow-right"
+              size={20}
+              color="#DDA15E"
+            />
+          </View>
+          <View style={styles.bottom}>
+            <Text style={styles.texteinfo}>
+              Je suis disponible pour m’occuper des plantes, ou entretenir des
+              jardins.
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.blocregister}>
+        <TouchableOpacity onPress={() => setShowModal(true)}>
+          <Text style={styles.register}>
+            Vous avez déjà un compte ? Connectez-vous
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +84,94 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#F6F5F1",
+  },
+  image: {
+    width: "70%",
+    height: "20%",
+  },
+
+  bloctext: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  textbienvenue: {
+    fontSize: 18,
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "26px",
+    color: "#283618",
+  },
+  textdemande: {
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: "32px",
+    lineHeight: "41px",
+    letterSpacing: "-0.03em",
+    color: "#283618",
+  },
+  recherche: {
+    borderColor: "#20232a",
+    borderStyle: "solid",
+    borderWidth: 1,
+    width: 300,
+    height: 150,
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+  },
+  top: {
+    flex: 1,
+    alignItems: "start-flex",
+    justifyContent: "center",
+    backgroundColor: "#F6F5F1",
+    flexDirection: "row",
+  },
+  titrerecherche: {
+    fontFamily: "Montserrat SemiBold",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: 21,
+    lineHeight: 26,
+    color: "#283618",
+    width: 250,
+    height: 250,
+  },
+  boutoncherche: {
+    fontFamily: "Montserrat SemiBold",
+    fontStyle: "normal",
+    fontWeight: "900",
+    fontSize: 25,
+    color: "#DDA15E",
+  },
+  bottom: {
+    flex: 1,
+    alignItems: "start-flex",
+    justifyContent: "center",
+  },
+  texteinfo: {
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: 15,
+    lineHeight: 17,
+    color: "#20232a",
+  },
+  blocregister: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 45,
+  },
+
+  register: {
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: 15,
+    lineHeight: 17,
+    color: "#20232a",
+    textDecorationLine: "underline",
   },
 });
