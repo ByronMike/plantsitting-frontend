77d95e99e83cd86cbe7d883f07320574aec26b48
Michael <auger.michaell@gmail.com>
2022-12-12 12:50:52 +0100

    all screens created

--- App.js
+++ App.js
@@ -12,9 +12,17 @@ import Presentation3Screen from './screens/Presentation3Screen';
 import Presentation4Screen from './screens/Presentation4Screen';
 import HomeScreen from './screens/HomeScreen';
 import SearchScreen from './screens/SearchScreen';
-
-
+import ScheduleScreen from './screens/ScheduleScreen';
+import MapScreen from './screens/MapScreen';
+// ! A intégrer dans MapScreen
+import ListingScreen from './screens/ListingScreen';
+import Plantsitter1Screen from './screens/Plantsitter1Screen';
+import Plantsitter2Screen from './screens/Plantsitter2Screen';
+import Plantsitter3Screen from './screens/Plantsitter3Screen';
 import SignupScreen from './screens/SignupScreen';
+import SummaryScreen from './screens/SummaryScreen';
+import CongratulationScreen from './screens/CongratulationScreen';
+import AssessmentScreen from './screens/AssessmentScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -42,9 +50,16 @@ export default function App() {
        <Stack.Screen name="Presentation4" component={Presentation4Screen} />
        <Stack.Screen name="Home" component={HomeScreen} />
        <Stack.Screen name="Search" component={SearchScreen} />
-
-
+       <Stack.Screen name="Schedule" component={ScheduleScreen} />
+       <Stack.Screen name="Map" component={MapScreen} />
+       <Stack.Screen name="Listing" component={ListingScreen} />
+       <Stack.Screen name="Plantsitter1" component={Plantsitter1Screen} />
+       <Stack.Screen name="Plantsitter2" component={Plantsitter2Screen} />
+       <Stack.Screen name="Plantsitter3" component={Plantsitter3Screen} />
        <Stack.Screen name="Signup" component={SignupScreen} />
+       <Stack.Screen name="Summary" component={SummaryScreen} />
+       <Stack.Screen name="Congratulation" component={CongratulationScreen} />
+       <Stack.Screen name="Assessment" component={AssessmentScreen} />
 
        <Stack.Screen name="TabNavigator" component={TabNavigator} />
      </Stack.Navigator>
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,39 +1,75 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function LandingScreen({ navigation }) {
- return (
-   <View style={styles.container}>
-     <Text>Landing Screen</Text>
-     <Button
-       title="Go to Presentation1 Screen"
-       onPress={() => navigation.navigate('Presentation1')}
-     />
-     <Button
-       title="Go to Presentation2 Screen"
-       onPress={() => navigation.navigate('Presentation2')}
-     />
-     <Button
-       title="Go to Presentation3 Screen"
-       onPress={() => navigation.navigate('Presentation3')}
-     />
-     <Button
-       title="Go to Presentation4 Screen"
-       onPress={() => navigation.navigate('Presentation4')}
-     />
-     <Button
-       title="Go to Home Screen"
-       onPress={() => navigation.navigate('Home')}
-     />
-     <Button
-       title="Go to Search Screen"
-       onPress={() => navigation.navigate('Search')}
-     />
-     <Button
-       title="Go to Sign-up Screen"
-       onPress={() => navigation.navigate('Signup')}
-     />
-   </View>
- );
+  return (
+    <View style={styles.container}>
+      <Text>Landing Screen</Text>
+      <Button
+        title="Go to Presentation1 Screen"
+        onPress={() => navigation.navigate("Presentation1")}
+      />
+      <Button
+        title="Go to Presentation2 Screen"
+        onPress={() => navigation.navigate("Presentation2")}
+      />
+      <Button
+        title="Go to Presentation3 Screen"
+        onPress={() => navigation.navigate("Presentation3")}
+      />
+      <Button
+        title="Go to Presentation4 Screen"
+        onPress={() => navigation.navigate("Presentation4")}
+      />
+      <Button
+        title="Go to Home Screen"
+        onPress={() => navigation.navigate("Home")}
+      />
+      <Button
+        title="Go to Search Screen"
+        onPress={() => navigation.navigate("Search")}
+      />
+      <Button
+        title="Go to Schedule Screen"
+        onPress={() => navigation.navigate("Schedule")}
+      />
+      <Button
+        title="Go to Map Screen"
+        onPress={() => navigation.navigate("Map")}
+      />
+      <Button
+        title="Go to Listing Screen"
+        onPress={() => navigation.navigate("Listing")}
+      />
+      <Button
+        title="Go to Plantsitter1 Screen"
+        onPress={() => navigation.navigate("Plantsitter1")}
+      />
+      <Button
+        title="Go to Plantsitter2 Screen"
+        onPress={() => navigation.navigate("Plantsitter2")}
+      />
+      <Button
+        title="Go to Plantsitter3 Screen"
+        onPress={() => navigation.navigate("Plantsitter3")}
+      />
+      <Button
+        title="Go to Sign-up Screen"
+        onPress={() => navigation.navigate("Signup")}
+      />
+      <Button
+        title="Go to Summary Screen"
+        onPress={() => navigation.navigate("Summary")}
+      />
+      <Button
+        title="Go to Congratulation Screen"
+        onPress={() => navigation.navigate("Congratulation")}
+      />
+      <Button
+        title="Go to Assessment Screen"
+        onPress={() => navigation.navigate("Assessment")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
