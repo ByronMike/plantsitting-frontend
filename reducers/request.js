7a6bc049cc594290049fc1ac458bef13765c3e7b
Michael <auger.michaell@gmail.com>
2022-12-15 14:47:21 +0100

    commit

--- reducers/request.js
+++ reducers/request.js
@@ -2,17 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: {
-    arrosage: false,
-    entretien: false,
-    traitement: false,
-    autre: false,
-    plantQty1: false,
-    plantQty5: false,
-    plantQty15: false,
-    garde: false,
-    depot: false,
-    startday: false,
-    endday: false,
+    // arrosage: false,
+    // entretien: false,
+    // traitement: false,
+    // autre: false,
+    // plantQty1: false,
+    // plantQty5: false,
+    // plantQty15: false,
+    // garde: false,
+    // depot: false,
+    // startday: null,
+    // endday: null,
   },
 };
 
@@ -22,21 +22,35 @@ export const requestsSlice = createSlice({
   initialState,
   reducers: {
     userRequest: (state, action) => {
-      state.value.arrosage = action.payload.arrosage;
-      state.value.entretien = action.payload.arrosage;
-      state.value.traitement = action.payload.arrosage;
-      state.value.autre = action.payload.arrosage;
-      state.value.plantQty1 = action.payload.plantQty1;
-      state.value.plantQty5 = action.payload.plantQty5;
-      state.value.plantQty15 = action.payload.plantQty15;
-      state.value.garde = action.payload.garde;
-      state.value.depot = action.payload.depot;
-      state.value.startday = action.payload.startday;
-      state.value.endday = action.payload.endday;
+        state.value = action.payload
+    //   state.value.arrosage = action.payload.arrosage;
+    //   state.value.entretien = action.payload.entretien;
+    //   state.value.traitement = action.payload.traitement;
+    //   state.value.autre = action.payload.autre;
+    //   state.value.plantQty1 = action.payload.plantQty1;
+    //   state.value.plantQty5 = action.payload.plantQty5;
+    //   state.value.plantQty15 = action.payload.plantQty15;
+    //   state.value.garde = action.payload.garde;
+    //   state.value.depot = action.payload.depot;
+    //   state.value.startday = action.payload.startday;
+    //   state.value.endday = action.payload.endday;
     },
-    // TODO (si on a le temps) Créer un reducer pour se désinscrire
+    cleanRequest: (state, action) => {
+    //   state.value.arrosage = false;
+    //   state.value.entretien = false;
+    //   state.value.traitement = false;
+    //   state.value.autre = false;
+    //   state.value.plantQty1 = false;
+    //   state.value.plantQty5 = false;
+    //   state.value.plantQty15 = false;
+    //   state.value.garde = false;
+    //   state.value.depot = false;
+    //   state.value.startday = false;
+    //   state.value.endday = false;
+    state.value = {}
+    },
   },
 });
 
-export const { userRequest } = requestsSlice.actions;
+export const { userRequest, cleanRequest } = requestsSlice.actions;
 export default requestsSlice.reducer;
